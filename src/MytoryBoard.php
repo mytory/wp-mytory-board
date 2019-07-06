3289b073a152bb2d9c751b6dda529a3aecaf37a7
An, Hyeong-woo <mytory@gmail.com>
2019-07-06 15:58:56 +0900

    미디어 페이지에서, 관리자/편집자가 아닌 경우 오직 자기 게시물만 나오게 처리함.

--- src/MytoryBoard.php
+++ src/MytoryBoard.php
@@ -74,6 +74,7 @@ class MytoryBoard {
 		add_action( "wp_ajax_{$this->taxonomyKey}_increase_pageview", [ $this, 'increasePageview' ] );
 		add_action( "wp_ajax_nopriv_{$this->taxonomyKey}_increase_pageview", [ $this, 'increasePageview' ] );
 		add_action( "publish_{$this->postTypeKey}", [ $this, 'defaultMytoryBoard' ] );
+		add_action( 'pre_get_posts', [ $this, 'onlyMyMedia' ] );
 
 		if ( $this->roleByBoard ) {
 			add_action( "create_{$this->taxonomyKey}", [ $this, 'addRole' ], 10, 2 );
@@ -357,6 +358,31 @@ class MytoryBoard {
 				]
 			);
 		}
+	}
+
+	public function onlyMyMedia( $wp_query_obj ) {
+
+		global $current_user, $pagenow;
+
+		$is_attachment_request = ( $wp_query_obj->get( 'post_type' ) == 'attachment' );
+
+		if ( ! $is_attachment_request ) {
+			return;
+		}
+
+		if ( ! is_a( $current_user, 'WP_User' ) ) {
+			return;
+		}
+
+		if ( ! in_array( $pagenow, array( 'upload.php', 'admin-ajax.php' ) ) ) {
+			return;
+		}
+
+		if ( ! current_user_can( 'delete_pages' ) ) {
+			$wp_query_obj->set( 'author', $current_user->ID );
+		}
+
+		return;
 	}
 }
 
